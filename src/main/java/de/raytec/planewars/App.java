b1431b039cf97a0811a572e1188aad1f678cf815
Rhiath <nlak@gmx.de>
2013-05-05 22:34:59 +0200

    - added some dependencies for libgdx - created a little sample that draws a circle

--- src/main/java/de/raytec/planewars/App.java
+++ src/main/java/de/raytec/planewars/App.java
@@ -1,13 +1,21 @@
 package de.raytec.planewars;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "my-gdx-game";
+        cfg.useGL20 = false;
+        cfg.width = 480;
+        cfg.height = 320;
+
+        new LwjglApplication(new MyGame(), cfg);
     }
 }
---
+++ src/main/java/de/raytec/planewars/MyGame.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.raytec.planewars;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+/**
+ *
+ * @author raymoon
+ */
+class MyGame implements ApplicationListener {
+
+    public MyGame() {
+    }
+
+    public void create() {
+        //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void resize(int i, int i1) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        ShapeRenderer renderer = new ShapeRenderer();
+        renderer.begin(ShapeType.Circle);
+        renderer.setColor(1, 1, 0, 1);
+        renderer.circle(50.0f, 60.0f, 30.0f, 20);
+        renderer.end();
+
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void pause() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void resume() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void dispose() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
